4fd42ea2a3f7277e7a73b1b49a3f06da60d4a7fe
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-28 18:42:24 -0700

    Add plugin marker for javadoc links plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,6 +95,7 @@ plugin("spotless", "org.incendo.cloudbuildlogic.SpotlessPlugin")
 plugin("spotless.root-project", "org.incendo.cloudbuildlogic.SpotlessRootProjectPlugin")
 plugin("publishing", "org.incendo.cloudbuildlogic.PublishingPlugin")
 plugin("publishing.root-project", "org.incendo.cloudbuildlogic.RootProjectPublishingPlugin")
+plugin("javadoc-links", "org.incendo.cloudbuildlogic.JavadocLinksPlugin")
 
 fun plugin(name: String, implClass: String) {
     val prefixedId = "org.incendo.cloud-build-logic.$name"
