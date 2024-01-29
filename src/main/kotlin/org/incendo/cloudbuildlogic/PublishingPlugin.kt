82da78de08d7c97ad07a8c5e1011438a4addfb41
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-28 20:43:37 -0700

    Apply crossdoc conventions only to published projects

--- src/main/kotlin/org/incendo/cloudbuildlogic/PublishingPlugin.kt
+++ src/main/kotlin/org/incendo/cloudbuildlogic/PublishingPlugin.kt
@@ -10,6 +10,7 @@ class PublishingPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply("net.kyori.indra.publishing")
         target.plugins.apply(JavadocLinksPlugin::class)
+        target.plugins.apply(CrossdocConventions::class)
 
         target.extensions.configure(IndraExtension::class) {
             signWithKeyFromProperties("signingKey", "signingPassword")
